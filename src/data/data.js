057f799c0928aad027b0e8872b0d8beccf84c510
anuj singh <anuj.singh229@gmail.com>
2025-07-01 00:17:40 -0400

    added project

--- src/data/data.js
+++ src/data/data.js
@@ -3,26 +3,28 @@ import RealEst from '../assets/projects/realestate.jpg'
 
 export const data=[
     {
-        id:1,
-        name:"React JS Application",
-        image:WorkIm,
-        github:"https://github.com/anuj229/portfoliowebiste",
-        live:"https://glittery-lollipop-0dae8e.netlify.app",
-    },
-    {
         id:2,
-        name:"React JS Application",
+        name:"Hotel Booking",
         image:RealEst,
         github:"https://github.com/anuj229/hotel-booking",
         live:"https://legendary-dango-64cbd4.netlify.app",
     },
     {
         id:3,
-        name:"React JS Application",
+        name:"E-commerce",
         image:WorkIm,
-        github:"",
-        live:"",
+        github:"https://github.com/anuj229/sb-ecom",
+        live:"https://creative-souffle-f814ce.netlify.app",
     },
+    {
+        id:1,
+        name:"E-commerce 2",
+        image:WorkIm,
+        github:"https://github.com/anuj229/portfoliowebiste",
+        live:"https://glittery-lollipop-0dae8e.netlify.app",
+    },
+
+  
     {
         id:4,
         name:"Vintage Car website",
